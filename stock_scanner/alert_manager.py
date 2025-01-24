d5da35f418ee020bc97ec664a76dcb46ad0363b2
Shivam Pandey <shivampandey4194@gmail.com>
2025-01-25 01:49:45 +0530

    .

--- stock_scanner/alert_manager.py
+++ stock_scanner/alert_manager.py
@@ -4,7 +4,9 @@ import time
 from datetime import datetime
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://%s:%s@%s.awl7l.mongodb.net/?retryWrites=true&w=majority&appName=%s' % (MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_CLUSTER, MONGODB_APPNAME))
+MONGO_CONN='mongodb+srv://%s:%s@%s.awl7l.mongodb.net/?retryWrites=true&w=majority&appName=%s' % (MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_CLUSTER, MONGODB_APPNAME)
+print(MONGO_CONN)
+client = MongoClient(MONGO_CONN)
 db = client['stock_alerts_db']
 alerts_collection = db['alerts_sent'] 
 
